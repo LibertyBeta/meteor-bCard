406bd3dd3ed859f780516606dc0338ae6584b1ff
Rainer Paskiewicz <rainer.paskiewicz@gmail.com>
2016-03-21 20:35:49 -0400

    First commit

---
+++ client/bCard/bCard.js
@@ -0,0 +1,11 @@
+Template.bCard.helpers({
+  firstName: function() {
+    return Session.get('firstName');
+  },
+  lastName: function() {
+    return Session.get('lastName');
+  },
+  quote: function(){
+    return Session.get("quote");
+  }
+})
---
+++ client/newForm/newForm.js
@@ -0,0 +1,24 @@
+Template.newForm.events({
+  "submit form": function(event, template) {
+
+    event.preventDefault();
+
+
+    let firstName = template.find("#firstName").value;
+    let lastName = template.find("#lastName").value;
+    let quote = template.find("#quote").value;
+    let gender = template.find("#gender").value;
+    let githubLink = template.find("#githubLink").value;
+
+    // session.set
+    // console.log(data);
+
+    Session.set('firstName', firstName);
+    Session.set('lastName', lastName);
+    Session.set('gender', gender);
+    Session.set('quote', quote);
+    Session.set('githubLink', githubLink);
+
+    Router.go(`/${firstName}`);
+  }
+});
